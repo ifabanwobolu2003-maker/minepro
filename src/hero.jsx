c8f501a3768489c9d9890dd1b4504f100d0b4f2c
Ifabanwo Boluwatife <ifabanwobolu2003@gmail.com>
2026-04-07 15:33:48 +0100

    Update hero.jsx

--- src/hero.jsx
+++ src/hero.jsx
@@ -71,6 +71,9 @@ export default function Example() {
         <div classname="lg:hidden">
          <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
           <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+            <div className="flex items-center justify-between">
+              {/* <a href="#" className="-m-1.5 p-1.5">
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
