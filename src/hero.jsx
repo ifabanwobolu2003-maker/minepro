06337d329b6a80f619e20c298d95d14e16e03ba1
Ifabanwo Boluwatife <ifabanwobolu2003@gmail.com>
2026-04-07 16:17:10 +0100

    Update hero.jsx

--- src/hero.jsx
+++ src/hero.jsx
@@ -44,7 +44,7 @@ export default function Example() {
 
         <div className='lg:hidden md:flex flex-col justify-end'>
             <button onClick={handleMenuToggle} className="text-white">
-              {isMenuOpen ? <XMarkIcon className="size-6" /> : <Bars3Icon className="size-6" />}
+              {isMenuOpen ? <XMarkIcon className="size-6" /> : <Bars3Icon className="size-9" />}
             </button>
         </div>
 
@@ -52,7 +52,7 @@ export default function Example() {
          <Dialog open={isMenuOpen} onClose={handleMenuToggle} className="lg:hidden">
             <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
-            <div className="fixed items-center justify-between">
+            <div className="flex items-center justify-between">
               <button
                 type="button"
                 onClick={() => setIsMenuOpen(false)}
