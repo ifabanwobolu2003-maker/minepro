d0895a01ccff2ca39b7ef65e2d79cdc6b1d69b49
Ifabanwo Boluwatife <ifabanwobolu2003@gmail.com>
2026-04-07 16:14:12 +0100

    Update hero.jsx

--- src/hero.jsx
+++ src/hero.jsx
@@ -52,7 +52,7 @@ export default function Example() {
          <Dialog open={isMenuOpen} onClose={handleMenuToggle} className="lg:hidden">
             <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
-            <div className="flex items-center justify-between">
+            <div className="fixed items-center justify-between">
               <button
                 type="button"
                 onClick={() => setIsMenuOpen(false)}
