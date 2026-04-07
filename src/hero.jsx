eadba83442c9102bd7ee1aa61666bc954d4b17ab
Ifabanwo Boluwatife <ifabanwobolu2003@gmail.com>
2026-04-07 15:50:56 +0100

    Update hero.jsx

--- src/hero.jsx
+++ src/hero.jsx
@@ -88,6 +88,29 @@ export default function Example() {
                 <XMarkIcon aria-hidden="true" className="size-6" />
               </button>
             </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-white/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
           </DialogPanel>   
          </Dialog> 
         </div>
